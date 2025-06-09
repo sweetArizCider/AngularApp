e6e573515d798341752ae70ba2cf36ac1b6083dc
applecider <arizah.applecider+github@gmail.com>
2025-06-09 00:07:03 -0600

    chores :construction: arizpe | works on products view

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
-import { Home } from '../views/home/home'; 
-import { FormView } from '../views/form/form';
+import { Home } from '@views/home/home'; 
+import { FormView } from '@views/form/form';
+import { ProductsView } from '@views/products/products.view';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'type', component: FormView },
+  { path: 'products', component: ProductsView },
   { path: '**', redirectTo: '' },
 ];
---
+++ src/app/views/products/products.view.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavBar } from '@components/navBar/navBar';
+
+@Component({
+  selector: 'products-view-selector',
+  templateUrl: './products.view.html',
+  styleUrl: './products.view.css',
+  imports: [NavBar],
+})
+
+export class ProductsView {
+
+}
